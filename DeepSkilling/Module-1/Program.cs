90006f78e3a17065e5d5db3e1cbab105987e57f9
Jagadish Sadineni <jagadeeshsadineni5@gmail.com>
2026-06-14 12:36:42 +0530

    Add files via upload

---
+++ DeepSkilling/Module-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public sealed class Logger
+{
+    private static Logger instance;
+
+    private Logger()
+    {
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        Console.WriteLine(
+            ReferenceEquals(logger1, logger2));
+    }
+}
